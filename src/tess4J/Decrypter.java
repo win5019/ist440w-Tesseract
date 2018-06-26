4ada48c51b1188ea8ece67d8f33d94ba3f9153e5
Win Ton <wton5019@gmail.com>
2018-06-26 00:51:29 -0400

    Importing dictionary is successful. Starting on Decrypter.

---
+++ src/tess4J/Decrypter.java
@@ -0,0 +1,19 @@
+package tess4J;
+
+import java.util.ArrayList;
+
+public class Decrypter {
+
+	public void decryptEnglishArrayList(ArrayList<String> encryptedArrayList) {
+		
+		// Replace all zeroes with o's, 5’s with s’s, and 6’s with G’s
+		// Create ArrayList with replaced 1’s with i’s
+		// Create ArrayList with replaced 1’s with L’s 
+		
+		
+		
+	}
+	
+	
+	
+}
